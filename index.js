56bdd8207055ab87331da74dbea542bac1944b60
quentin czarnynoga <czarnynoga.quentins@gmail.com>
2023-08-03 16:24:48 +0200

    Anim JS

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-let scrollAnim = document.querySelectorAll(".js-scroll");
-let scrollElem = scrollElements.forEach((el) => {
-    el.style.opacity = 0
-  })
+const slidingGauche = document.querySelector('.slide-in');
+const slideIn = slidingGauche.classList.add('active');
+
+const slidingPropos = document.querySelector('.slide-inP');
+const slidInP = slidingPropos.classList.add('active');
+
+const slidingExp = document.querySelector('.slide-inE');
+const slidInE = slidingExp.classList.add('active');
+
+const slidingPro = document.querySelector('.slide-inPr');
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+    const topElementToTopViewport = slidingPro.getBoundingClientRect().top;
+    console.log(topElementToTopViewport);
+    if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5) {
+        slidingPro.classList.add('active')
+    }
+})
