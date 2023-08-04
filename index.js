8c8665ab30574243d8f62aff2e9d172bd2440ab2
quentin czarnynoga <czarnynoga.quentins@gmail.com>
2023-08-04 11:58:20 +0200

    fin JS début querries

--- index.js
+++ index.js
@@ -8,12 +8,21 @@ const slidingExp = document.querySelector('.slide-inE');
 const slidInE = slidingExp.classList.add('active');
 
 const slidingPro = document.querySelector('.slide-inPr');
-
 window.addEventListener('scroll', () => {
     const { scrollTop, clientHeight } = document.documentElement;
     const topElementToTopViewport = slidingPro.getBoundingClientRect().top;
     console.log(topElementToTopViewport);
     if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.5) {
         slidingPro.classList.add('active')
+    }
+})
+
+const slidingFormulaire = document.querySelector('.slide-inForm');
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+    const topElementToTopViewport = slidingFormulaire.getBoundingClientRect().top;
+    console.log(topElementToTopViewport);
+    if (scrollTop > (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.75) {
+        slidingFormulaire.classList.add('active')
     }
 })
